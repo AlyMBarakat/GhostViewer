8e020395047889b5f074f0da4115a477a0da235d
AlyMBarakat <aly.m.barakat@gmail.com>
2020-06-01 05:31:06 +0200

    Added stream view and edited join

--- Views/join/join.js
+++ Views/join/join.js
@@ -1,15 +1,30 @@
 const electron = require("electron");
-
-
-
-
+let BrowserWindow = electron.remote.BrowserWindow; //Remote object from main process
+const path = require("path");	//Built in path module
+const url = require("url");		//Built in url modul
 const joinBtn = document.getElementById('joinBtn');
+//URLs
+let workURL = { //index HTML file object
+	pathname: path.join(__dirname, '../stream/index.html'),
+	protocol: 'file',
+	slashes: true
+}
 
-//On button click: get captcha and reques join
+//Create window using remote module
+function createRendererWindow(urlObj) {
+	let remoteWin = new BrowserWindow();  //Remote object of Browser Window
+	remoteWin.setFullScreen(true);
+	remoteWin.loadURL(url.format(urlObj)); //send IPC message
+	remoteWin.webContents.openDevTools();
+}
+
+//On button click: get captcha and request join
 joinBtn.addEventListener('click', function() { 
 	console.log("join-request");
 	let captcha = document.getElementById('captchaInput').value; //Input captcha form user
 	console.log(captcha);
+	
+	createRendererWindow(workURL);
 	/*
 	*
 	* Request joint WebRTC
